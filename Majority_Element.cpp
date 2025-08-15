91fc4e1ce6431d469d67cf1d4679410c03ce8eb7
SM Thahir <155128623+thahir02@users.noreply.github.com>
2025-08-15 22:44:18 +0530

    Create Majority_Element.cpp

---
+++ Majority_Element.cpp
@@ -0,0 +1,43 @@
+/*
+Problem :- 
+Majority Element 
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+Answer :-
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int maj=nums[0];
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(count==0)
+            {
+                maj=nums[i];
+                count++;
+                continue;
+            }
+            if(maj==nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return maj;
+    }
+};
