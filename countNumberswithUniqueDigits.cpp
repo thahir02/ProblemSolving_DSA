53599c16f46508d1a21b3d32656b855ff29d1d42
SM Thahir <155128623+thahir02@users.noreply.github.com>
2025-11-22 21:41:28 +0530

    Add function to count unique digit numbers
    Implement a solution to count numbers with unique digits based on the given integer n. The function handles edge cases and iteratively calculates the count.

---
+++ countNumberswithUniqueDigits.cpp
@@ -0,0 +1,33 @@
+/*
+Problem :
+Count Numbers with Unique Digits
+Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n.
+
+Example 1:
+Input: n = 2
+Output: 91
+Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+
+Example 2:
+Input: n = 0
+Output: 1
+
+Constraints:
+0 <= n <= 8
+
+Answer :
+*/
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+       if(n==0){
+        return 1;
+       } 
+       int ans=10,cnt=9;
+       for(int i=0;i<n-1;i++){
+        cnt*=(9-i);
+        ans+=cnt;
+       }
+       return ans;
+    }
+};
