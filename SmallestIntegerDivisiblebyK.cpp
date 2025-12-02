ec68d0cfc9899f40b4504f79d4e7915dc5584394
SM Thahir <155128623+thahir02@users.noreply.github.com>
2025-12-02 20:50:21 +0530

    Add solution for smallest integer divisible by K
    Implement solution to find smallest integer divisible by k consisting only of digit 1.

---
+++ SmallestIntegerDivisiblebyK.cpp
@@ -0,0 +1,45 @@
+/*
+Problem :
+Smallest Integer Divisible by K
+Given a positive integer k, you need to find the length of the smallest positive integer n such that n is divisible by k, and n only contains the digit 1.
+
+Return the length of n. If there is no such n, return -1.
+
+Note: n may not fit in a 64-bit signed integer.
+
+Example 1:
+Input: k = 1
+Output: 1
+Explanation: The smallest answer is n = 1, which has length 1.
+
+Example 2:
+Input: k = 2
+Output: -1
+Explanation: There is no such positive integer n divisible by 2.
+
+Example 3:
+Input: k = 3
+Output: 3
+Explanation: The smallest answer is n = 111, which has length 3.
+ 
+
+Constraints:
+1 <= k <= 10^5
+
+Answer
+*/
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k%2==0)
+        {
+            return -1;
+        }
+        int n=0,i=0;
+        while((i++)<=k){
+            n=(n*10+1)%k;
+            if(!n) return i;
+        }
+        return -1;
+    }
+};
